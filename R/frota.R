f4ffb0e6a458998b31a561344c8791cf8c7c578b
pabsantos <pabsantos21@gmail.com>
2023-08-14 16:57:52 -0300

    Sintetiza a frota por combustivel

--- R/frota.R
+++ R/frota.R
@@ -1,3 +1,38 @@
+arrange_frota_combustivel <- function(tabela_combustivel) {
+  class_alcool <- c("ALCOOL", "ALCOOL/GAS NATURAL COMBUSTIVEL")
+  class_gnv <- c(
+    "ALCOOL/GAS NATURAL VEICULAR",
+    "DIESEL/GAS NATURAL VEICULAR", 
+    "GAS NATURAL VEICULAR", 
+    "GASOLINA/ALCOOL/GAS NATURAL",
+    "GASOLINA/GAS NATURAL VEICULAR"
+  )
+  class_flex <- "ALCOOL/GASOLINA"
+  class_diesel <- c("DIESEL", "DIESEL/GAS NATURAL COMBUSTIVEL")
+  class_eletrico <- c(
+    "ELETRICO/FONTE EXTERNA", 
+    "ELETRICO/FONTE INTERNA", 
+    "GASOLINA/ALCOOL/ELETRICO", 
+    "GASOLINA/ELETRICO"
+  )
+  class_gasolina <- c("GASOL/GAS NATURAL COMBUSTIVEL", "GASOLINA")
 
-frota_combustivel |>
-  group_by()
+  frota_agrupado <- tabela_combustivel |>
+    mutate(combustivel = case_match(
+      combustivel,
+      class_alcool ~ "Álcool",
+      class_gnv ~ "GNV",
+      class_flex ~ "Flex",
+      class_diesel ~ "Diesel",
+      class_eletrico ~ "Elétrico",
+      class_gasolina ~ "Gasolina",
+      .default = NA
+    )) |>
+    group_by(ano, uf, combustivel) |>
+    summarise(quantidade = sum(quantidade)) |>
+    ungroup() |>
+    drop_na()
+
+  return(frota_agrupado)
+}
+
--- R/main.R
+++ R/main.R
@@ -3,3 +3,5 @@ library(readxl)
 library(janitor)
 
 load("data/frota_combustivel.rda")
+
+frota_combustivel_sintese <- arrange_frota_combustivel(frota_combustivel)
